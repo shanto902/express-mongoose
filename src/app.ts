a87a33aeb49b6747426dcd2d3da12816de96a367
Ashik Ali Shanto <shanto902@gmail.com>
2023-11-23 22:18:11 +0600

    Project setup dont

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import express, { Application, Request, Response } from 'express'
-const app : Application = express()
-const port = 3000
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
 
-app.get('/', ( req: Request, res: Response ) => {
-  res.send('Hello World!')
-})
+// parsers
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
 
 export default app;
