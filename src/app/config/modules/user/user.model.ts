32b21321485f61a7eafd5107024746348574ab49
Ashik Ali Shanto <shanto902@gmail.com>
2023-11-24 01:19:06 +0600

    1st Step validation complete

--- src/app/config/modules/user/user.model.ts
+++ src/app/config/modules/user/user.model.ts
@@ -2,26 +2,36 @@ import { Schema, model } from 'mongoose';
 import { TAddress, TFullName, TUser } from './user.interface';
 
 const userNameSchema = new Schema<TFullName>({
-  firstName: { type: String },
-  lastName: { type: String },
+  firstName: { type: String, required: [true, 'First Name Required'] },
+  lastName: { type: String, required: [true, 'Last Name Required'] },
 });
 
 const userAddressSchema = new Schema<TAddress>({
-  street: { type: String },
-  city: { type: String },
-  country: { type: String },
+  street: { type: String, required: [true, 'Street Required'] },
+  city: { type: String, required: [true, 'City Required'] },
+  country: { type: String, required: [true, 'Country Required'] },
 });
 
 const userSchema = new Schema<TUser>({
-  userId: { type: Number },
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-  fullName: userNameSchema,
-  age: { type: Number },
-  email: { type: String },
-  isActive: { type: Boolean },
+  userId: { type: Number, unique: true },
+  username: {
+    type: String,
+    required: [true, 'Username Required'],
+    unique: true,
+  },
+  password: { type: String, required: [true, 'Password Required'] },
+  fullName: {
+    type: userNameSchema,
+    required: [true, 'Full Name Required'],
+  },
+  age: { type: Number, required: [true, 'Age Required'] },
+  email: { type: String, required: [true, 'Email Required'], unique: true },
+  isActive: { type: Boolean, default: true },
   hobbies: [{ type: String }],
-  address: userAddressSchema,
+  address: {
+    type: userAddressSchema,
+    required: [true, 'Address Required'],
+  },
 });
 
 export const UserModel = model<TUser>('User', userSchema);
